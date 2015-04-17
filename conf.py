f8d98d1fa449610f01a87425b427406267c9f64c
Jim Anderson <jim@smartthings.com>
2015-04-17 14:52:48 -0500

    add favicon

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SmartThings Documentation'
-copyright = u'2014, SmartThings'
+copyright = u'2015, SmartThings'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -129,7 +129,7 @@ html_theme = 'default'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
