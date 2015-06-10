40d09a61c8457a66509c087c1ce4d33c50abf6cf
Jim Anderson <jim@smartthings.com>
2015-06-10 15:24:38 -0500

    cleanups for latex builds

--- conf.py
+++ conf.py
@@ -203,7 +203,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'SmartThingsDocumentation.tex', u'SmartThings Documentation Documentation',
+  ('index', 'SmartThingsDocumentation.tex', u'SmartThings Developer Documentation',
    u'SmartThings', 'manual'),
 ]
 
